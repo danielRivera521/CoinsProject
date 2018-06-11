625a51aca8eb7f858f849ee7aa3c5312c24c7658
Daniel Rivera <drivera1@fullsail.edu>
2018-06-11 13:51:56 -0400

    Initial Commit

---
+++ ChoresForCoinsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChoresForCoinsProject
+//
+//  Created by Daniel Rivera on 6/11/18.
+//  Copyright © 2018 Daniel Rivera, Andrew Harrington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }   
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '9.0'
+
+target 'ChoresForCoinsProject' do
+  use_frameworks!
+
+  # Pods for ChoresForCoinsProject
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Storage'
+pod 'Firebase/Database'
+
+end
